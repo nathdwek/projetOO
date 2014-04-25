7f59ea1f43987f402039e43e19f09cf95cb74b93
jun <bluh>
2014-04-25 16:18:02 +0200

    JE CHANGE DES LIGNES BLANCHES BECAUSE FUCK IT

--- src/sonic/model/Monster.java
+++ src/sonic/model/Monster.java
@@ -3,19 +3,20 @@ import sonic.view.Drawable;
 
 public abstract class Monster extends Hittable implements SelfUpdatable  {
 	private Point speed;
-	
+
 	public Monster(Point position, Point speed){
 		super(position);
 		this.speed= speed;
 	}
-	
+
 	public Point getSpeed(){
 		return speed;
 	}
 	public void setSpeed(Point s){
 		speed=s;
 	}
-	
+
+
 	public void handleCollision(Hittable otherHittable) {
 		// TODO Auto-generated method stub
 
