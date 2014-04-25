a22fb6e331f0e2f0e0f3631d610408f991a9bb3c
jun <bluh>
2014-04-25 14:18:36 +0200

    hittable

--- src/sonic/model/Hittable.java
+++ src/sonic/model/Hittable.java
@@ -1,5 +1,16 @@
 package sonic.model;
-public interface Hittable {
+import sonic.view.Drawable;
+public abstract class Hittable implements Drawable {
+	private Point position;
+	
+	public Hittable(Point position){
+		this.position= position;
+		
+	}
+	
+	public Point getPosition(){
+		return position;
+	}
 
 	public void handleCollision(Hittable otherHittable);
 
